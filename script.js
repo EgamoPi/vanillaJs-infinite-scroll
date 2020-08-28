d3f27f6234e590335724b654be0b169fdc39db4a
j.sekretev@icloud.com <j.sekretev@icloud.com>
2020-08-28 11:16:12 +0000

    added logic for infinite scroll and added custom loader

--- script.js
+++ script.js
@@ -2,12 +2,25 @@
 const imagaContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let isReady = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
-const count = 10;
+const count = 30;
 const apiKey = 'NvA_JpQXDQSaq-pPsiQyKd-bRpyJeT44-UGYtcZRgec';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Check if all images were loaded
+function imageLoaded(){
+    imagesLoaded++;
+    console.log(imagesLoaded)
+    if (imagesLoaded === totalImages){
+        isReady = true;
+        loader.hidden = true;
+        console.log('ready = ', isReady);
+    }
+}
 //Helper function to set att on DOM elements
 function setAtt(element, atts){
     for(const key in atts){
@@ -18,6 +31,9 @@ function setAtt(element, atts){
 
 //Crzate Elements for Links & Photos, add to DOM
 function displayPhotos(){
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    console.log('total images= ', totalImages)
     // Run function for each object in photosArray
     photosArray.forEach((photo) => {
         // create <a> to link to unsplash
@@ -32,7 +48,9 @@ function displayPhotos(){
             src: photo.urls.regular,
             alt: photo.alt_description,
             title: photo.alt_description,
-        })
+        });
+        //Event listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded);
         //put img -> a, then return -> imagaContainer element
         item.appendChild(img);
         imagaContainer.appendChild(item);
@@ -41,7 +59,7 @@ function displayPhotos(){
 
 
 //Get random photos from API
-async function getPhoto(){
+async function getPhotos(){
     try {
         const response = await fetch(apiUrl);
         photosArray = await response.json();
@@ -51,5 +69,13 @@ async function getPhoto(){
     }
 }
 
+//Check to see if soon to bottom
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && isReady) {
+        isReady = false;
+        getPhotos();
+    }
+})
+
 //On load
-getPhoto();
+getPhotos();
